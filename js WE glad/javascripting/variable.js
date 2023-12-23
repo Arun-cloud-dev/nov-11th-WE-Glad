0acfea46eb036cc7d52bd90125e5fecc2cf13cf5
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2023-12-23 18:35:25 +0530

    Comit main

---
+++ js WE glad/javascripting/variable.js
@@ -0,0 +1,166 @@
+//auto
+
+
+// x = 5;
+// y = 6;
+
+// x = 10
+
+// z = x + y; 
+// console.log(z);
+
+
+// var , let , const 
+
+
+// var x = 5;
+// var y = 10;
+
+// z = x + y;
+
+
+// let x = 5;
+// var y = 10;
+ 
+// const z = x + y;
+
+
+// const x = 56
+//     ;
+// const y = 45;
+// const c = x + y;
+
+
+// const X = 65;
+
+
+
+
+// const price1 = 5;
+// const price2 = 56;
+
+// let total = price1 + price2;
+
+// console.log(total);
+
+
+
+// var ar_r = 565;
+
+
+
+
+// const array and onject 
+
+
+
+// {
+//     let x = 2;
+// }
+
+
+
+// let x = 'Ar';
+// {
+
+// let x = 0;
+
+// }
+
+
+// var x = 4;
+
+// {
+//     var x = 2;
+//     console.log(x);
+
+// }
+
+
+
+
+// let x = 10;
+// {
+//     let x = 2;
+//     console.log(x);
+// }
+
+// console.log(x);
+
+
+
+// const x = 10;
+// {
+//     const x = 55;
+// }
+
+// console.log(x);
+
+
+
+// var x = 2;
+// let  x = 3; //not alowed 
+
+
+// {
+//     let x = 12;
+
+// }
+
+// {
+//     let x = 56;
+//     var x = 25;
+
+// }
+
+
+// console.log(x);
+
+
+//
+// const PI = 3.4654321684546512554354;
+//  var PI = PI + 10;
+
+// console.log(PI);
+
+
+// const PI = 56;
+
+
+// let A;
+// A = 56;
+
+
+
+
+// const:
+// //  new array 
+// objects
+
+// new function
+
+
+
+
+// let cars = ["ar", "vlovo", "BMW"];
+// {
+// let cars = "Audi";
+// }
+// console.log(cars);
+
+
+
+// const arfam = { type: "indian", member: "4", continent: "Asia" }
+
+
+// arfam.continent= "AUS";
+
+// arfam.language = "tamil";
+
+// console.log(arfam);
+
+
+
+
+
+
